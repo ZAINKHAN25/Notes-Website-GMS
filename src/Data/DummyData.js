d400f914fef4a139d3ffbc056f0d303b756ef8b4
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-08-23 20:27:25 +0500

    Some changes in dummy data.

--- src/Data/DummyData.js
+++ src/Data/DummyData.js
@@ -1,25 +1,55 @@
 const dummyData = [
-    {
-        class: 10,
-        subjects: [
-            {
-                subject: "Biology",
-                chapters: [
-                    {
-                        chaptorNum: 1,
-                        chaptorName: "Gaseous Excahange",
-                        pages: [
-                            { pageNum: 1, pageSrc: "ImgUrl", pageName: "Introduction", notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-                            { pageNum: 2, pageSrc: "ImgUrl", pageName: "Gases and their properties", notes: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-                            { pageNum: 3, pageSrc: "ImgUrl", pageName: "Combustion", notes: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
-                            { pageNum: 4, pageSrc: "ImgUrl", pageName: "Boiling and freezing points", notes: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
-                            { pageNum: 5, pageSrc: "ImgUrl", pageName: "Gaseous state", notes: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-                        ]
-                    }
-                ]
-            }
-        ] 
-    }
+  {
+    class: 10,
+    subjects: [
+      {
+        subject: "Biology",
+        chapters: [
+          {
+            chaptorNum: 1,
+            chaptorName: "Gaseous Excahange",
+            pages: [
+              {
+                pageNum: 1,
+                pageSrc: "ImgUrl",
+                pageName: "Introduction",
+                notes:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+              },
+              {
+                pageNum: 2,
+                pageSrc: "ImgUrl",
+                pageName: "Gases and their properties",
+                notes:
+                  "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+              },
+              {
+                pageNum: 3,
+                pageSrc: "ImgUrl",
+                pageName: "Combustion",
+                notes:
+                  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              },
+              {
+                pageNum: 4,
+                pageSrc: "ImgUrl",
+                pageName: "Boiling and freezing points",
+                notes:
+                  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+              },
+              {
+                pageNum: 5,
+                pageSrc: "ImgUrl",
+                pageName: "Gaseous state",
+                notes:
+                  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export default dummyData;
